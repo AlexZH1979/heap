6b0d6e82e8ce3d93a286d22b56a608bfa9c5ab65
AlexZH1979 <zhmyd@yandex.ru>
2016-01-20 12:02:31 +0300

    Rewrite overload memory

--- src/main/java/ru/zhmyd/App.java
+++ src/main/java/ru/zhmyd/App.java
@@ -1,11 +1,5 @@
 package ru.zhmyd;
 
-import java.lang.management.ManagementFactory;
-import java.lang.management.MemoryPoolMXBean;
-import java.lang.management.MemoryType;
-import java.lang.management.MemoryUsage;
-import java.util.Iterator;
-
 public class App {
     public static void main(String[] args) {
        try {
--- src/main/java/ru/zhmyd/MemoryListener.java
+++ src/main/java/ru/zhmyd/MemoryListener.java
@@ -2,7 +2,6 @@ package ru.zhmyd;
 
 import java.lang.management.ManagementFactory;
 import java.lang.management.MemoryPoolMXBean;
-import java.lang.management.MemoryType;
 import java.lang.management.MemoryUsage;
 import java.util.Iterator;
 
